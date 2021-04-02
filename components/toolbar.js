4b2506d9cff7a5bc1875403b783f81b3e88aff41
Ella <72365100+eilla1@users.noreply.github.com>
2021-04-01 23:45:46 -0700

    :fire: remove eom page

--- components/toolbar.js
+++ components/toolbar.js
@@ -8,7 +8,6 @@ export const Toolbar = () => {
         <div className={styles.main}>
             <div onClick={() => router.push('/')}>Home</div>
             <div onClick={() => router.push('/feed/1')}>Feed</div>
-            <div onClick={() => router.push('/eom')}>EOM</div>
             <div onClick={() => window.location.href = 'https://github.com'}>Source</div>
         </div>
     )
